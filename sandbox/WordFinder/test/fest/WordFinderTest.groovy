9367fff60c12c252bf07490c5fe6b38d4e16e887
shemnon <shemnon@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-02-10 02:56:59 +0000

    Really, it was on the shelf a minute ago!
    git-svn-id: https://svn.codehaus.org/griffon/core/trunk@463 bb902e69-0157-0410-a63b-ba7c3dd83122

--- sandbox/WordFinder/test/fest/WordFinderTest.groovy
+++ sandbox/WordFinder/test/fest/WordFinderTest.groovy
@@ -1,22 +1,22 @@
 import java.awt.Dimension
 import org.fest.swing.fixture.*
 import org.testng.annotations.*
-import griffon.application.StandaloneApplication
+import griffon.application.SingleFrameApplication
 import griffon.util.GriffonApplicationHelper
 
 class WordFinderTest {
-   private StandaloneApplication app
+   private SingleFrameApplication app
    private FrameFixture window
 
    static {
-      StandaloneApplication.metaClass.shutdown = { ->
+      SingleFrameApplication.metaClass.shutdown = { ->
          GriffonApplicationHelper.runScriptInsideEDT("Shutdown", delegate)
          delegate.appFrames[0].visible = false
       }
    }
 
    @BeforeMethod void init() {
-      app = new StandaloneApplication()
+      app = new SingleFrameApplication()
       app.bootstrap()
       app.realize()
       window = new FrameFixture( app.appFrames[0] )
