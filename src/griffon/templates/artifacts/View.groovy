fc9a5c60c42b2e82765f6ed1aa4c8453ac787e25
shemnon <shemnon@bb902e69-0157-0410-a63b-ba7c3dd83122>
2009-05-07 05:56:08 +0000

    Java 5 icon should be 48x48 not 16x16
    git-svn-id: https://svn.codehaus.org/griffon/core/trunk@654 bb902e69-0157-0410-a63b-ba7c3dd83122

--- src/griffon/templates/artifacts/View.groovy
+++ src/griffon/templates/artifacts/View.groovy
@@ -3,7 +3,7 @@
   pack:true,
   //location:[50,50],
   locationByPlatform:true,
-  iconImage: imageIcon('/griffon-icon-16x16.png').image,
+  iconImage: imageIcon('/griffon-icon-48x48.png').image,
   iconImages: [imageIcon('/griffon-icon-48x48.png').image,
                imageIcon('/griffon-icon-32x32.png').image,
                imageIcon('/griffon-icon-16x16.png').image]
