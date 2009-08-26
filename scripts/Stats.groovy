3255aab9bb539548ce7c1bcab1b9b05a2b917ebc
Andres Almiray <aalmiray@yahoo.com>
2009-08-25 19:04:30 -0700

    Updated stats script to produce a better LOC number. It is now able to recognize single and multiple line comments, it also takes into account additional sources like griffon-app/wizards and src/clojure

--- scripts/Stats.groovy
+++ scripts/Stats.groovy
@@ -24,73 +24,95 @@
 includeTargets << griffonScript("_GriffonSettings")
 
 target (stats: "Generates basic stats for a Griffon project") {
+    def EMPTY = /^\s*$/
+    def SLASH_SLASH = /^\s*\/\/.*/
+    def SLASH_STAR_STAR_SLASH = /^(.*)\/\*(.*)\*\/(.*)$/
 
-	// maps file path to
-	def pathToInfo = [
-	      new Expando(name: "Controllers", filetype: ".groovy", path: "controllers"),
-	      new Expando(name: "Models", filetype: ".groovy", path: "models"),
-	      new Expando(name: "Views", filetype: ".groovy", path: "views"),
-//	      new Expando(name: "Domain Classes", filetype: ".groovy", path: "domain"),
-//	      new Expando(name: "Jobs", filetype: ".groovy", path: "jobs"),
-	      new Expando(name: "Services", filetype: ".groovy", path: "services"),
-	      new Expando(name: "Lifecycle", filetype: ".groovy", path: "lifecycle"),
-	      new Expando(name: "Groovy Source", filetype: ".groovy", path: "src.main"),
-	      new Expando(name: "Java Source", filetype: ".java", path: "src.main"),
-	      new Expando(name: "Unit Tests", filetype: ".groovy", path: "test.unit"),
-          new Expando(name: "Integration Tests", filetype: ".groovy", path: "test.integration"),
-          new Expando(name: "Scripts", filetype: ".groovy", path: "scripts"),
-	]
+    // TODO - handle slash_star comments inside strings
+    def DEFAULT_LOC_MATCHER = { file ->
+        loc = 0
+        comment = 0
+        file.eachLine { line ->
+            if(line ==~ EMPTY) return
+            else if(line ==~ SLASH_SLASH) return
+            else {
+                def m = line =~ SLASH_STAR_STAR_SLASH
+                if(m.count && m[0][1] ==~ EMPTY && m[0][3] ==~ EMPTY) return
+                int open = line.indexOf("/*")
+                int close = line.indexOf("*/")
+                if(open != -1 && (close-open) <= 1) comment++
+                else if(close != -1 && comment) comment--
+            }
+            if(!comment) loc++
+        } 
+        loc
+    }
 
+    // TODO - find a way to make paths pluggable, i.e. wizards, clojure, etc.
 
-	new File(basedir).eachFileRecurse { file ->
+    // maps file path to
+    def pathToInfo = [
+        [name: "Models",              path: "models",           filetype: [".groovy"]],
+        [name: "Views",               path: "views",            filetype: [".groovy"]],
+        [name: "Controllers",         path: "controllers",      filetype: [".groovy"]],
+        [name: "Lifecycle",           path: "lifecycle",        filetype: [".groovy"]],
+        [name: "Services",            path: "services",         filetype: [".groovy"]],
+        [name: "Wizards",             path: "wizards",          filetype: [".groovy"]],
+        [name: "Groovy/Java Sources", path: "src.main",         filetype: [".groovy",".java"]],
+        [name: "Common Sources",      path: "src.commons",      filetype: [".groovy",".java"]],
+        [name: "Clojure Sources",     path: "src.clojure",      filetype: [".clj"], locmatcher: {file ->
+            def loc = 0
+            file.eachLine { line ->
+                if(line ==~ EMPTY || line ==~ /^\s*\;.*/) return
+                loc++
+            }
+            loc
+        }],
+        [name: "JavaFX Sources",      path: "src.javafx",       filetype: [".fx"]],
+        [name: "Scala Sources",       path: "src.scala",        filetype: [".scala"]],
+        [name: "Unit Tests",          path: "test.unit",        filetype: [".groovy"]],
+        [name: "Integration Tests",   path: "test.integration", filetype: [".groovy"]],
+        [name: "Scripts",             path: "scripts",          filetype: [".groovy"]],
+    ]
 
-		def match = pathToInfo.find { expando ->
-			file.path =~ expando.path &&
-			file.path.endsWith(expando.filetype)
-		}
-		if (match && file.isFile() ) {
+    new File(basedir).eachFileRecurse { file ->
+        def match = pathToInfo.find { info ->
+            file.path =~ info.path &&
+            info.filetype.any{ s -> file.path.endsWith(s) }
+            //file.path.endsWith(info.filetype)
+        }
+        if (match && file.isFile() ) {
+            match.filecount = match.filecount ? match.filecount+1 : 1
+            // strip whitespace
+            loc = match.locmatcher ? match.locmatcher(file) : DEFAULT_LOC_MATCHER(file)
+            match.loc = match.loc ? match.loc + loc : loc
+        }
+    }
 
-			if (/*file.path.toLowerCase() =~ /web-inf/ ||*/ file.path.toLowerCase() =~ /plugins/) {
-				// println "Skipping $file.path in WEB-INF or plugins dir"
-			} else {
-				match.filecount = match.filecount ? match.filecount+1 : 1
-				// strip whitespace
-				def loc = file.readLines().findAll { line -> !(line ==~ /^\s*$/) }.size()
-				match.loc = match.loc ? match.loc + loc : loc
-			}
-		}
+    def totalFiles = 0
+    def totalLOC = 0
 
-	}
+    println '''
+    +----------------------+-------+-------+
+    | Name                 | Files |  LOC  |
+    +----------------------+-------+-------+'''
 
+    pathToInfo.each { info ->
 
-	def totalFiles = 0
-	def totalLOC = 0
+        if (info.filecount) {
+            println "    | " +
+                info.name.padRight(20," ") + " | " +
+                info.filecount.toString().padLeft(5, " ") + " | " +
+                info.loc.toString().padLeft(5," ") + " | "
+            totalFiles += info.filecount
+            totalLOC += info.loc
+        }
 
-	println '''
-	+----------------------+-------+-------+
-	| Name                 | Files |  LOC  |
-	+----------------------+-------+-------+'''
+    }
 
-
-	pathToInfo.each { info ->
-
-		if (info.filecount) {
-			println "	| " +
-				info.name.padRight(20," ") + " | " +
-				info.filecount.toString().padLeft(5, " ") + " | " +
-				info.loc.toString().padLeft(5," ") + " | "
-			totalFiles += info.filecount
-			totalLOC += info.loc
-		}
-
-	}
-
-
-	println "	+----------------------+-------+-------+"
-	println "	| Totals               | " + totalFiles.toString().padLeft(5, " ") + " | " + totalLOC.toString().padLeft(5, " ") + " | "
-	println "	+----------------------+-------+-------+\n"
-
-
+    println "    +----------------------+-------+-------+"
+    println "    | Totals               | " + totalFiles.toString().padLeft(5, " ") + " | " + totalLOC.toString().padLeft(5, " ") + " | "
+    println "    +----------------------+-------+-------+\n"
 }
 
 setDefaultTarget(stats)
