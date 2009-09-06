6d9148fad229a98c4215c1b20a1398aa87d8fdac
shemnon <danno.ferrin@shemnon.com>
2009-09-05 21:36:16 -0600

    GRIFFON-79 - menu bar integration on map only works for mac look and feel.  So we will prefer mac look and feel by default.

--- samples/SwingPad/griffon-app/lifecycle/Initialize.groovy
+++ samples/SwingPad/griffon-app/lifecycle/Initialize.groovy
@@ -2,4 +2,4 @@ import groovy.swing.SwingBuilder
 import griffon.util.GriffonPlatformHelper
 
 GriffonPlatformHelper.tweakForNativePlatform(app)
-SwingBuilder.lookAndFeel('nimbus', 'mac', 'gtk', ['metal', [boldFonts: false]])
+SwingBuilder.lookAndFeel('mac', 'nimbus', 'gtk', ['metal', [boldFonts: false]])
