29d0b210b68826d41956a345a646b89628725fb9
aalmiray <aalmiray@gmail.com>
2010-06-27 12:54:09 +0200

    quick&dirty way to check if buld is successful at codehaus' bamboo. take 3

--- src/cli/griffon/beans/Listener.java
+++ src/cli/griffon/beans/Listener.java
@@ -20,7 +20,7 @@ import org.codehaus.groovy.transform.GroovyASTTransformationClass;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
