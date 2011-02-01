1b11500458f160c3f5c48f76985e7dbab10c6379
aalmiray <aalmiray@gmail.com>
2011-02-01 11:39:15 +0100

    Fix javadoc on @Threading, @EventPublisher and @Listener

--- src/main/cli/griffon/util/Threading.java
+++ src/main/cli/griffon/util/Threading.java
@@ -24,23 +24,56 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotates a method or property.<p>
+ * <p>Annotates a method or property.</p>
  *
  * Annotated elements must follow these rules
  * <ul>
- *  <li>must be a public method or property</li>
- *  <li>name does not match an event handler</li>
- *  <li>must pass {@code griffon.util.GriffonClassUtils.isPlainMethod()} if it's a method</li>
- *  <li>its value must be a closure (including curried method pointers) if it's a property</li>
+ * <li>must be a public method or property</li>
+ * <li>name does not match an event handler</li>
+ * <li>must pass {@code griffon.util.GriffonClassUtils.isPlainMethod()} if it's a method</li>
+ * <li>its value must be a closure (including curried method pointers) if it's a property</li>
  * </ul>
  *
  * This annotation takes {@code griffon.util.Threading.Policy} as value, with {@code Threading.Policy.OUTSIDE_UITHREAD} being
- * the default value.
+ * the default value.<p>
  *
- * @see griffon.util.ThreadingPolicy
- * @see org.codehaus.griffon.ast.ThreadingASTTransformation
+ * <p>The following snippet exemplifies the compactness of code when the annotation is applied </p>
+ * <pre>
+ * import griffon.util.Threading
+ *
+ * class Sample {
+ *     &#064;Threading
+ *     void doSomethingOutside(String arg) {
+ *         println "Outside $arg"
+ *     }
+ *     &#064;Threading(Threading.Policy.INSIDE_UITHREAD_SYNC)
+ *     void doSomethingInside(String arg) {
+ *         println "Inside $arg"
+ *     }
+ * }
+ * </pre>
+ *
+ * <p>The equivalent, non-annotated code is</p>
+ * <pre>
+ * import griffon.util.UIThreadHelper
+ *
+ * class Sample {
+ *     void doSomethingOutside(String arg) {
+ *         UIThreadHelper.instance.executeOutside {
+ *             println "Outside $arg"
+ *         }
+ *     }
+ *     void doSomethingInside(String arg) {
+ *         UIThreadHelper.instance.executeSync {
+ *             println "Inside $arg"
+ *         }
+ *     }
+ * }
+ * </pre>
  *
  * @author Andres Almiray
+ * @see griffon.util.Threading.Policy
+ * @see org.codehaus.griffon.ast.ThreadingASTTransformation
  * @since 0.9.2
  */
 @Retention(RetentionPolicy.SOURCE)
@@ -53,25 +86,32 @@ public @interface Threading {
      * Indicates the type of threading management for a method or property.</p>
      * The following values apply
      * <ul>
-     *  <li>{@code SKIP} - no threading management will be performed.</li>
-     *  <li>{@code OUTSIDE_UITHREAD} - code should be invoked outside of the UI thread.</li>
-     *  <li>{@code INSIDE_UITHREAD_SYNC} - code should be invoked inside the UI thread using a synchronous call.</li>
-     *  <li>{@code INSIDE_UITHREAD_ASYNC} - code should be invoked inside the UI thread using an asynchronous call.</li>
+     * <li>{@code SKIP} - no threading management will be performed.</li>
+     * <li>{@code OUTSIDE_UITHREAD} - code should be invoked outside of the UI thread.</li>
+     * <li>{@code INSIDE_UITHREAD_SYNC} - code should be invoked inside the UI thread using a synchronous call.</li>
+     * <li>{@code INSIDE_UITHREAD_ASYNC} - code should be invoked inside the UI thread using an asynchronous call.</li>
      * </ul>
      *
-     * @see griffon.util.Threading
-     *
      * @author Andres Almiray
+     * @see griffon.util.Threading
      * @since 0.9.2
      */
     public enum Policy {
-        /** Skip threading injection */
+        /**
+         * Skip threading injection
+         */
         SKIP,
-        /** Inject execOutside wrapper */
+        /**
+         * Inject execOutside wrapper
+         */
         OUTSIDE_UITHREAD,
-        /** Inject execSync wrapper */
+        /**
+         * Inject execSync wrapper
+         */
         INSIDE_UITHREAD_SYNC,
-        /** Inject execAsync wrapper */
+        /**
+         * Inject execAsync wrapper
+         */
         INSIDE_UITHREAD_ASYNC;
     }
 }
