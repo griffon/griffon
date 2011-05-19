ab2e0fbf974bb81d9c6099e5ca2818eceae4ebfa
Rene Groeschke <rene@breskeby.com>
2011-05-19 11:43:02 +0200

    moved more tests to cli module

---
+++ subprojects/cli/src/test/groovy/griffon/util/RunModeTests.groovy
@@ -0,0 +1,41 @@
+package griffon.util
+
+class RunModeTests extends GroovyTestCase {
+    protected void setUp() {
+        System.clearProperty(RunMode.KEY)
+    }
+
+    protected void tearDown() {
+        System.clearProperty(RunMode.KEY)
+    }
+
+    void testIsSystemSet() {
+        assert !RunMode.isSystemSet()
+        System.setProperty(RunMode.KEY, 'STANDALONE')
+        assert RunMode.isSystemSet()
+    }
+
+    void testGetCurrent() {
+        assert RunMode.current == RunMode.STANDALONE
+
+        System.setProperty(RunMode.KEY, 'STANDALONE')
+        assert RunMode.current == RunMode.STANDALONE
+
+        System.setProperty(RunMode.KEY, 'WEBSTART')
+        assert RunMode.current == RunMode.WEBSTART
+
+        System.setProperty(RunMode.KEY, 'APPLET')
+        assert RunMode.current == RunMode.APPLET
+
+        System.setProperty(RunMode.KEY, 'TEST RUN MODE')
+        assert RunMode.currentRunMode == RunMode.CUSTOM
+        assert RunMode.current.name == 'TEST RUN MODE'
+    }
+
+    void testGetRunMode() {
+        assert RunMode.STANDALONE == RunMode.getRunMode('standalone')
+        assert RunMode.WEBSTART == RunMode.getRunMode('webstart')
+        assert RunMode.APPLET == RunMode.getRunMode('applet')
+        assert !RunMode.getRunMode('custom')
+    }
+}
