210b9e005235952c08f05218da8dddde120388d3
Marc Paquette <marcpa@mac.com>
2012-05-17 08:21:22 -0400

    --bootstrap at command line results in a Boolean in argsMap : handle this case also.

--- subprojects/griffon-scripts/src/main/groovy/RunScript.groovy
+++ subprojects/griffon-scripts/src/main/groovy/RunScript.groovy
@@ -26,7 +26,7 @@ includeTargets << griffonScript('_GriffonBootstrap')
 
 target(name: 'runScript', description: 'Execute the specified script(s) after starting up the application environment',
         prehook: null, posthook: null) {
-    boolean doBootstrap = argsMap.bootstrap && Boolean.parseBoolean(argsMap.bootstrap)
+    boolean doBootstrap = argsMap.bootstrap && (argsMap.bootstrap instanceof Boolean ?: Boolean.parseBoolean(argsMap.bootstrap))
     if (doBootstrap) {
         depends(bootstrap)
     } else {
