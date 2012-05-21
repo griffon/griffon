4e22b9ffb9d1e4708377ab0da629de4f398f7d3a
aalmiray <aalmiray@gmail.com>
2012-05-21 10:12:57 +0200

    Update Upgrade sctipt to handle 0.9.5 and upwards

--- subprojects/griffon-scripts/src/main/groovy/Upgrade.groovy
+++ subprojects/griffon-scripts/src/main/groovy/Upgrade.groovy
@@ -64,116 +64,150 @@ target(upgrade: "Upgrades a Griffon application from a previous version of Griff
     if (isArchetypeProject) projectType = 'archetype'
     createStructure()
 
-    griffonUnpack(dest: basedir, src: "griffon-shared-files.jar")
-    ant.unzip(src: "${basedir}/griffon-wrapper-files.zip", dest: basedir)
-    ant.delete(file: "${basedir}/griffon-wrapper-files.zip", quiet: true)
-
-    File upgradeDir = new File("${basedir}/upgrade")
-    ant.mkdir(dir: upgradeDir)
-    File inBuildConfigFile = new File("${basedir}/griffon-app/conf/BuildConfig.groovy")
-    File inConfigFile = new File("${basedir}/griffon-app/conf/Config.groovy")
-    File inBuilderConfigFile = new File("${basedir}/griffon-app/conf/Builder.groovy")
-    File outBuildConfigFile = new File("${upgradeDir}/BuildConfig.groovy")
-    File outConfigFile = new File("${upgradeDir}/Config.groovy")
-    File outBuilderConfigFile = new File("${upgradeDir}/Builder.groovy")
-
-    if (inBuildConfigFile.exists() && !outBuildConfigFile.exists()) ant.move(file: inBuildConfigFile, tofile: outBuildConfigFile)
-    if (inConfigFile.exists() && !outConfigFile.exists()) ant.move(file: inConfigFile, tofile: outConfigFile)
-    if (inBuilderConfigFile.exists() && !outBuilderConfigFile.exists()) ant.move(file: inBuilderConfigFile, tofile: outBuilderConfigFile)
-
-    // Unpack the shared files into a temporary directory
-    File tmpdir = new File("${basedir}/tmp-upgrade")
-    griffonUnpack(dest: tmpdir.path, src: "griffon-${projectType}-files.jar")
-
-    try {
-        switch (projectType) {
-            case 'app':
-                ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/BuildConfig.groovy")
-                ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/Config.groovy")
-                ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/Builder.groovy")
-
-                def value = outBuildConfigFile.text =~ /.*app.fileType = (.*)/
-                if (value) inBuildConfigFile.append("""\n${value[0][0]}\n""")
-                value = outBuildConfigFile.text =~ /.*app.defaultPackageName = (.*)/
-                if (value) inBuildConfigFile.append("""\n${value[0][0]}\n""")
-
-                // copy new icons to griffon-app/conf/webstart
-                // copy new icons to griffon-app/resources
-                ant.copy(todir: "${basedir}") {
-                    fileset(dir: tmpdir.path, includes: "**/*.png")
-                }
-                break
-            case 'plugin':
-                ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/BuildConfig.groovy")
-                break
-            case 'archetype':
-                ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/BuildConfig.groovy")
-        }
-
-        ant.replace(dir: "${basedir}/griffon-app/conf", includes: "**/*.*") {
-            replacefilter(token: "@griffonAppName@", value: capitalize(griffonAppName))
-            replacefilter(token: "@griffonAppVersion@", value: griffonAppVersion ?: "0.1")
-        }
-    } finally {
-        delete(dir: tmpdir.path)
-    }
-
-    if (!isArchetypeProject) {
-        // remove GriffonApplicationHelper from Initialize.groovy
-        def initializeFile = new File(basedir, '/griffon-app/lifecycle/Initialize.groovy')
-        if (initializeFile.exists()) {
-            initializeFile.text -= 'import griffon.util.GriffonPlatformHelper\n'
-            initializeFile.text -= 'GriffonPlatformHelper.tweakForNativePlatform(app)\n'
-        }
-
-        touch(file: "${basedir}/griffon-app/i18n/messages.properties")
-
+    if (appGriffonVersion == '0.9.5') {
         event("StatusUpdate", ["Updating application.properties"])
+        def props = new Properties()
+        props.load(new File("${basedir}/application.properties").newReader())
         propertyfile(file: "${basedir}/application.properties",
                 comment: "Do not edit app.griffon.* properties, they may change automatically. " +
                         "DO NOT put application configuration in here, it is not the right place!") {
             entry(key: "app.name", value: griffonAppName)
             entry(key: "app.griffon.version", value: griffonVersion)
-            if (!isPluginProject) {
-                entry(key: "plugins.swing", value: '0.9.5')
+            if (!isArchetypeProject && !isPluginProject) {
                 entry(key: "archetype.default", value: griffonVersion)
-                entry(key: "app.toolkit", value: 'swing')
+            }
+            if (props['plugins.swing'] == '0.9.5') {
+                entry(key: "plugins.swing", value: '1.0.0')
             }
         }
-    }
 
-    def wrapperConfig = new File(basedir, '/wrapper/griffon-wrapper.properties')
-    if (wrapperConfig.exists()) {
-        def wrapperProps = new Properties()
-        wrapperConfig.eachLine {l ->
-            if (l.startsWith('#')) return
-            List kv = l.tokenize('=')
-            kv[1] = kv[1].replace('\\', '')
-            wrapperProps.put(kv[0], kv[1])
+        def wrapperConfig = new File(basedir, '/wrapper/griffon-wrapper.properties')
+        if (wrapperConfig.exists()) {
+            def wrapperProps = new Properties()
+            wrapperConfig.eachLine {l ->
+                if (l.startsWith('#')) return
+                List kv = l.tokenize('=')
+                kv[1] = kv[1].replace('\\', '')
+                kv[1] = kv[1].replace(appGriffonVersion, griffonVersion)
+                wrapperProps.put(kv[0], kv[1])
+            }
+            wrapperConfig.withOutputStream { o ->
+                wrapperProps.store(o, "Griffon $griffonVersion upgrade")
+            }
         }
-        wrapperProps.put('distributionVersion', griffonVersion)
-        wrapperConfig.withOutputStream { o ->
-            wrapperProps.store(o, "Griffon $griffonVersion upgrade")
-        }
-    }
+        printFramed(" Project ${griffonAppName} has been upgraded.")
+    } else { // older
+        griffonUnpack(dest: basedir, src: "griffon-shared-files.jar")
+        ant.unzip(src: "${basedir}/griffon-wrapper-files.zip", dest: basedir)
+        ant.delete(file: "${basedir}/griffon-wrapper-files.zip", quiet: true)
 
-    printFramed("""
-        | Project ${griffonAppName} has been upgraded.
-        |
-        | Griffon ${griffonVersion} brings substantial changes to logging
-        | and packaging configuration. The upgrade process has taken the
-        | precuation of making copies of your configuration files and placed
-        | them inside
-        |
-        |   ${upgradeDir.absolutePath}
-        |
-        | Please review and copy over any project specific values into their
-        | respective configuration files, taking into account that builder
-        | configuration will be set automatically as plugins are installed.
-        |
-        | Also, review that the values of 'application.properties' are
-        | appropriate for the current project.
-        |""".stripMargin())
+        File upgradeDir = new File("${basedir}/upgrade")
+        ant.mkdir(dir: upgradeDir)
+        File inBuildConfigFile = new File("${basedir}/griffon-app/conf/BuildConfig.groovy")
+        File inConfigFile = new File("${basedir}/griffon-app/conf/Config.groovy")
+        File inBuilderConfigFile = new File("${basedir}/griffon-app/conf/Builder.groovy")
+        File outBuildConfigFile = new File("${upgradeDir}/BuildConfig.groovy")
+        File outConfigFile = new File("${upgradeDir}/Config.groovy")
+        File outBuilderConfigFile = new File("${upgradeDir}/Builder.groovy")
+
+        if (inBuildConfigFile.exists() && !outBuildConfigFile.exists()) ant.move(file: inBuildConfigFile, tofile: outBuildConfigFile)
+        if (inConfigFile.exists() && !outConfigFile.exists()) ant.move(file: inConfigFile, tofile: outConfigFile)
+        if (inBuilderConfigFile.exists() && !outBuilderConfigFile.exists()) ant.move(file: inBuilderConfigFile, tofile: outBuilderConfigFile)
+
+        // Unpack the shared files into a temporary directory
+        File tmpdir = new File("${basedir}/tmp-upgrade")
+        griffonUnpack(dest: tmpdir.path, src: "griffon-${projectType}-files.jar")
+
+        try {
+            switch (projectType) {
+                case 'app':
+                    ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/BuildConfig.groovy")
+                    ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/Config.groovy")
+                    ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/Builder.groovy")
+
+                    def value = outBuildConfigFile.text =~ /.*app.fileType = (.*)/
+                    if (value) inBuildConfigFile.append("""\n${value[0][0]}\n""")
+                    value = outBuildConfigFile.text =~ /.*app.defaultPackageName = (.*)/
+                    if (value) inBuildConfigFile.append("""\n${value[0][0]}\n""")
+
+                    // copy new icons to griffon-app/conf/webstart
+                    // copy new icons to griffon-app/resources
+                    ant.copy(todir: "${basedir}") {
+                        fileset(dir: tmpdir.path, includes: "**/*.png")
+                    }
+                    break
+                case 'plugin':
+                    ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/BuildConfig.groovy")
+                    break
+                case 'archetype':
+                    ant.copy(todir: "${basedir}/griffon-app/conf", file: "${tmpdir}/griffon-app/conf/BuildConfig.groovy")
+            }
+
+            ant.replace(dir: "${basedir}/griffon-app/conf", includes: "**/*.*") {
+                replacefilter(token: "@griffonAppName@", value: capitalize(griffonAppName))
+                replacefilter(token: "@griffonAppVersion@", value: griffonAppVersion ?: "0.1")
+            }
+        } finally {
+            delete(dir: tmpdir.path)
+        }
+
+        if (!isArchetypeProject) {
+            // remove GriffonApplicationHelper from Initialize.groovy
+            def initializeFile = new File(basedir, '/griffon-app/lifecycle/Initialize.groovy')
+            if (initializeFile.exists()) {
+                initializeFile.text -= 'import griffon.util.GriffonPlatformHelper\n'
+                initializeFile.text -= 'GriffonPlatformHelper.tweakForNativePlatform(app)\n'
+            }
+
+            touch(file: "${basedir}/griffon-app/i18n/messages.properties")
+
+            event("StatusUpdate", ["Updating application.properties"])
+            propertyfile(file: "${basedir}/application.properties",
+                    comment: "Do not edit app.griffon.* properties, they may change automatically. " +
+                            "DO NOT put application configuration in here, it is not the right place!") {
+                entry(key: "app.name", value: griffonAppName)
+                entry(key: "app.griffon.version", value: griffonVersion)
+                if (!isPluginProject) {
+                    entry(key: "plugins.swing", value: '1.0.0')
+                    entry(key: "archetype.default", value: griffonVersion)
+                    entry(key: "app.toolkit", value: 'swing')
+                }
+            }
+        }
+
+        def wrapperConfig = new File(basedir, '/wrapper/griffon-wrapper.properties')
+        if (wrapperConfig.exists()) {
+            def wrapperProps = new Properties()
+            wrapperConfig.eachLine {l ->
+                if (l.startsWith('#')) return
+                List kv = l.tokenize('=')
+                kv[1] = kv[1].replace('\\', '')
+                kv[1] = kv[1].replace(appGriffonVersion, griffonVersion)
+                wrapperProps.put(kv[0], kv[1])
+            }
+            wrapperConfig.withOutputStream { o ->
+                wrapperProps.store(o, "Griffon $griffonVersion upgrade")
+            }
+        }
+
+        printFramed("""
+            | Project ${griffonAppName} has been upgraded.
+            |
+            | Griffon ${griffonVersion} brings substantial changes to logging
+            | and packaging configuration. The upgrade process has taken the
+            | precuation of making copies of your configuration files and placed
+            | them inside
+            |
+            |   ${upgradeDir.absolutePath}
+            |
+            | Please review and copy over any project specific values into their
+            | respective configuration files, taking into account that builder
+            | configuration will be set automatically as plugins are installed.
+            |
+            | Also, review that the values of 'application.properties' are
+            | appropriate for the current project.
+            |""".stripMargin())
+    }
 
     event("StatusFinal", ["Project upgraded"])
 }
