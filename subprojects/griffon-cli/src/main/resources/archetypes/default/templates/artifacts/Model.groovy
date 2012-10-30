68bd935ed4f3478a1e58962f5d2fc6bb53806dea
aalmiray <aalmiray@gmail.com>
2012-10-30 19:37:50 +0100

    Tweak artifact templates with init/destroy hints

--- subprojects/griffon-cli/src/main/resources/archetypes/default/templates/artifacts/Model.groovy
+++ subprojects/griffon-cli/src/main/resources/archetypes/default/templates/artifacts/Model.groovy
@@ -2,4 +2,12 @@
 
 class @artifact.name@ {
    // @Bindable String propName
+
+    // void mvcGroupInit(Map args) {
+    //    // this method is called after the model is instantiated
+    // }
+
+    // void mvcGroupDestroy() {
+    //    // this method is called when the group is destroyed
+    // }
 }
