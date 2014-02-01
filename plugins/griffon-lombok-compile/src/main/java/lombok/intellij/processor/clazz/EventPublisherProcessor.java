b48cb3006734275b2aa91dc7895a0d81ad4ccf79
aalmiray <aalmiray@gmail.com>
2014-02-01 16:48:12 +0100

    Fix broken headers in griffon-lombok-compile

--- plugins/griffon-lombok-compile/src/main/java/lombok/intellij/processor/clazz/EventPublisherProcessor.java
+++ plugins/griffon-lombok-compile/src/main/java/lombok/intellij/processor/clazz/EventPublisherProcessor.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- */
 package lombok.intellij.processor.clazz;
 
 import com.intellij.psi.*;
--- plugins/griffon-lombok-compile/src/main/java/lombok/intellij/processor/field/VetoableProcessor.java
+++ plugins/griffon-lombok-compile/src/main/java/lombok/intellij/processor/field/VetoableProcessor.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- */
 package lombok.intellij.processor.field;
 
 import com.intellij.psi.PsiAnnotation;
