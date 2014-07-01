336d7f247185ac117b625822dba54a21dd2e633f
aalmiray <aalmiray@gmail.com>
2014-07-01 16:50:05 +0200

    Simplify Config class in Java based templates

--- templates/griffon-pivot-java-templates/templates/griffon-pivot-java/griffon-app/conf/Config.java
+++ templates/griffon-pivot-java-templates/templates/griffon-pivot-java/griffon-app/conf/Config.java
@@ -1,22 +1,22 @@
 import griffon.util.AbstractMapResourceBundle;
-import griffon.util.CollectionUtils;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
 
 import static java.util.Arrays.asList;
+import static griffon.util.CollectionUtils.map;
 
 public class Config extends AbstractMapResourceBundle {
     @Override
     protected void initialize(@Nonnull Map<String, Object> entries) {
-        CollectionUtils.map(entries)
-            .e("application", CollectionUtils.map()
+        map(entries)
+            .e("application", map()
                 .e("title", "${project_name}")
                 .e("startupGroups", asList("${project_property_name}"))
                 .e("autoShutdown", true)
             )
-            .e("mvcGroups", CollectionUtils.map()
-                .e("${project_property_name}", CollectionUtils.map()
+            .e("mvcGroups", map()
+                .e("${project_property_name}", map()
                     .e("model", "${project_package}.${project_class_name}Model")
                     .e("view", "${project_package}.${project_class_name}View")
                     .e("controller", "${project_package}.${project_class_name}Controller")
