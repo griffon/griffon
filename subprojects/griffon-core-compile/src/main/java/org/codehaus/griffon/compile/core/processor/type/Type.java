e8795c0ae14c488158f49a8085d17f7916d5b6b2
aalmiray <aalmiray@gmail.com>
2014-11-20 16:53:42 +0100

    Added a type processor that registers metadata on any type at META-INF/types

---
+++ subprojects/griffon-core-compile/src/main/java/org/codehaus/griffon/compile/core/processor/type/Type.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2008-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.griffon.compile.core.processor.type;
+
+import org.kordamp.jipsy.processor.LogLocation;
+import org.kordamp.jipsy.processor.Logger;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Andres Almiray
+ * @since 2.1.0
+ */
+public final class Type {
+    private final Logger logger;
+    private final String typeName;
+    private final Set<String> providers = new LinkedHashSet<>();
+
+    public Type(Logger logger, String name) {
+        if (logger == null) {
+            throw new NullPointerException("logger");
+        }
+        if (name == null) {
+            throw new NullPointerException("name");
+        }
+        this.logger = logger;
+        logger.note(LogLocation.LOG_FILE, "Creating " + name);
+        this.typeName = name;
+    }
+
+    public void addProvider(String provider) {
+        if (provider == null) {
+            throw new NullPointerException("provider");
+        }
+        logger.note(LogLocation.LOG_FILE, "Adding " + provider + " to " + typeName);
+        providers.add(provider);
+    }
+
+    public boolean contains(String provider) {
+        return providers.contains(provider);
+    }
+
+    public boolean removeProvider(String provider) {
+        if (providers.remove(provider)) {
+            logger.note(LogLocation.LOG_FILE, "Removing " + provider + " from " + typeName);
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return typeName;
+    }
+
+    public String toProviderNamesList() {
+        StringBuilder sb = new StringBuilder();
+        List<String> names = new ArrayList<>(providers);
+        Collections.sort(names);
+        for (String provider : names) {
+            sb.append(provider).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public void fromProviderNamesList(String input) {
+        if (input == null) {
+            throw new NullPointerException("input");
+        }
+        String[] lines = input.split("\\n");
+        for (String line : lines) {
+            String[] content = line.split("#");
+            if (content.length > 0) {
+                String trimmed = content[0].trim();
+                if (trimmed.length() > 0) {
+                    addProvider(trimmed);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return typeName + "=" + providers;
+    }
+}
---
+++ subprojects/griffon-groovy-compile/src/main/java/org/codehaus/griffon/compile/core/ast/type/TypeProviderASTTransformation.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2008-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.griffon.compile.core.ast.type;
+
+import griffon.metadata.TypeProviderFor;
+import org.codehaus.griffon.compile.core.processor.type.Type;
+import org.codehaus.griffon.compile.core.processor.type.TypeCollector;
+import org.codehaus.groovy.ast.AnnotationNode;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.ast.ModuleNode;
+import org.codehaus.groovy.ast.expr.ClassExpression;
+import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.control.CompilePhase;
+import org.codehaus.groovy.transform.ASTTransformation;
+import org.codehaus.groovy.transform.GroovyASTTransformation;
+import org.kordamp.gipsy.transform.GipsyASTTransformation;
+import org.kordamp.jipsy.ServiceProviderFor;
+import org.kordamp.jipsy.processor.CheckResult;
+import org.kordamp.jipsy.processor.LogLocation;
+import org.kordamp.jipsy.processor.Persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.reflect.Modifier.isPublic;
+import static java.lang.reflect.Modifier.isStatic;
+
+/**
+ * @author Andres Almiray
+ * @since 2.1.0
+ */
+@ServiceProviderFor(ASTTransformation.class)
+@GroovyASTTransformation(phase = CompilePhase.CLASS_GENERATION)
+public class TypeProviderASTTransformation extends GipsyASTTransformation {
+    public static final String NAME = TypeProviderASTTransformation.class.getName()
+        + " (" + TypeProviderASTTransformation.class.getPackage().getImplementationVersion() + ")";
+
+    private static final ClassNode SERVICE_PROVIDER_FOR_TYPE = makeClassSafe(TypeProviderFor.class);
+
+
+    private Persistence persistence;
+    private TypeCollector data;
+
+    @Override
+    protected ClassNode getAnnotationClassNode() {
+        return SERVICE_PROVIDER_FOR_TYPE;
+    }
+
+    @Override
+    protected void initialize(ModuleNode moduleNode) {
+        super.initialize(moduleNode);
+
+        File outputDir = moduleNode.getContext().getConfiguration().getTargetDirectory();
+        persistence = new TypePersistence(NAME, options.dir(), outputDir, logger);
+        data = new TypeCollector(persistence.getInitializer(), logger);
+
+        // Initialize if possible
+        for (String typeName : persistence.tryFind()) {
+            data.getType(typeName);
+        }
+        //data.cache();
+    }
+
+    @Override
+    protected void removeStaleData(ClassNode classNode, ModuleNode moduleNode) {
+        data.removeProvider(classNode.getName());
+    }
+
+    protected void handleAnnotations(ClassNode classNode, List<AnnotationNode> annotations, ModuleNode moduleNode) {
+        CheckResult checkResult = checkCurrentClass(classNode);
+        if (checkResult.isError()) {
+            addError(checkResult.getMessage(), classNode, moduleNode.getContext());
+            return;
+        }
+
+        for (ClassNode type : findTypes(annotations)) {
+            CheckResult implementationResult = isImplementation(classNode, type);
+            if (implementationResult.isError()) {
+                addError(implementationResult.getMessage(), classNode, moduleNode.getContext());
+            } else {
+                register(type.getName(), classNode);
+            }
+        }
+    }
+
+    @Override
+    protected void writeData() {
+        // if (data.isModified()) {
+        if (data.types().isEmpty()) {
+            logger.note(LogLocation.LOG_FILE, "Writing output");
+            try {
+                persistence.delete();
+            } catch (IOException e) {
+                logger.warning(LogLocation.LOG_FILE, "An error occurred while deleting data file");
+            }
+        } else {
+            logger.note(LogLocation.LOG_FILE, "Writing output");
+            for (Type type : data.types()) {
+                try {
+                    persistence.write(type.getName(), type.toProviderNamesList());
+                } catch (IOException e) {
+                    // TODO print out error
+                }
+            }
+            persistence.writeLog();
+        }
+        // }
+    }
+
+    private CheckResult checkCurrentClass(ClassNode currentClass) {
+        if (currentClass.isEnum()) {
+            return CheckResult.valueOf("is not a class nor an interface");
+        }
+        if (!isPublic(currentClass.getModifiers())) {
+            return CheckResult.valueOf("is not public");
+        }
+
+        if (isStatic(currentClass.getModifiers())) {
+            return CheckResult.valueOf("is static");
+        }
+
+        return CheckResult.OK;
+    }
+
+    private List<ClassNode> findTypes(List<AnnotationNode> annotations) {
+        List<ClassNode> types = new ArrayList<>();
+
+        for (AnnotationNode annotation : annotations) {
+            for (Expression expr : findCollectionValueMember(annotation, "value")) {
+                if (expr instanceof ClassExpression) {
+                    types.add(((ClassExpression) expr).getType());
+                }
+            }
+        }
+
+        return types;
+    }
+
+    private void register(String typeName, ClassNode provider) {
+        data.getType(typeName).addProvider(provider.getName());
+    }
+}
