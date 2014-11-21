8f880b7010eb174769b4ab4549f79b8352e41ea2
aalmiray <aalmiray@gmail.com>
2014-11-21 11:48:59 +0100

    Add a Map to Properties adapter; allow application configuration to be transformed to a Properties instance

--- subprojects/griffon-core/src/main/java/griffon/util/CollectionUtils.java
+++ subprojects/griffon-core/src/main/java/griffon/util/CollectionUtils.java
@@ -15,7 +15,24 @@
  */
 package griffon.util;
 
-import java.util.*;
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * <p>Utility class that simplifies creating collections in Java.</p>
@@ -112,6 +129,21 @@ public final class CollectionUtils {
 
     public static <E> SetBuilder<E> set(Set<E> delegate) {
         return new SetBuilder<>(delegate);
+    }
+
+    /**
+     * Returns an adapted Map as a Properties instance.
+     * <p>
+     * The Map is used live, which means changes made to it will affect the
+     * Properties instance directly.
+     *
+     * @param map the Map instance ot adapt as a Properties instance
+     * @return a new Properties instance backed by the supplied Map.
+     */
+    @Nonnull
+    public static Properties toProperties(@Nonnull Map<String, Object> map) {
+        requireNonNull(map, "Argument 'map' must not be null");
+        return new MapToPropertiesAdapter(map);
     }
 
     public static class MapBuilder<K, V> implements Map<K, V> {
@@ -387,6 +419,147 @@ public final class CollectionUtils {
         @Override
         public String toString() {
             return delegate.toString();
+        }
+    }
+
+    private static class MapToPropertiesAdapter extends Properties {
+        private final Map<String, Object> map;
+
+        private MapToPropertiesAdapter(@Nonnull Map<String, Object> map) {
+            this.map = map;
+        }
+
+        @Override
+        public synchronized Object setProperty(String key, String value) {
+            return map.put(key, value);
+        }
+
+        @Override
+        public String getProperty(String key) {
+            Object value = map.get(key);
+            return value != null ? String.valueOf(value) : null;
+        }
+
+        @Override
+        public String getProperty(String key, String defaultValue) {
+            Object value = map.get(key);
+            return value != null ? String.valueOf(value) : defaultValue;
+        }
+
+        @Override
+        public Enumeration<?> propertyNames() {
+            return keys();
+        }
+
+        @Override
+        public Set<String> stringPropertyNames() {
+            return map.keySet();
+        }
+
+        @Override
+        public synchronized int size() {
+            return map.size();
+        }
+
+        @Override
+        public synchronized boolean isEmpty() {
+            return map.isEmpty();
+        }
+
+        @Override
+        public synchronized Enumeration<Object> keys() {
+            return new Enumeration<Object>() {
+                private Iterator<String> keys = new ArrayList<>(map.keySet()).iterator();
+
+                @Override
+                public boolean hasMoreElements() {
+                    return keys.hasNext();
+                }
+
+                @Override
+                public String nextElement() {
+                    return keys.next();
+                }
+            };
+        }
+
+        @Override
+        public synchronized Enumeration<Object> elements() {
+            return new Enumeration<Object>() {
+                private Iterator<Object> values = new ArrayList<>(map.values()).iterator();
+
+                @Override
+                public boolean hasMoreElements() {
+                    return values.hasNext();
+                }
+
+                @Override
+                public Object nextElement() {
+                    return values.next();
+                }
+            };
+        }
+
+        @Override
+        public synchronized boolean contains(Object value) {
+            return map.containsValue(value);
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return map.containsValue(value);
+        }
+
+        @Override
+        public synchronized boolean containsKey(Object key) {
+            return map.containsKey(key);
+        }
+
+        @Override
+        public synchronized Object get(Object key) {
+            return map.get(key);
+        }
+
+        @Override
+        public synchronized Object put(Object key, Object value) {
+            return map.put(String.valueOf(key), value);
+        }
+
+        @Override
+        public synchronized Object remove(Object key) {
+            return map.remove(key);
+        }
+
+        @Override
+        public synchronized void putAll(Map<?, ?> t) {
+            map.putAll((Map<String, Object>) t);
+        }
+
+        @Override
+        public synchronized void clear() {
+            map.clear();
+        }
+
+        @Override
+        public Set<Object> keySet() {
+            return new LinkedHashSet<Object>(map.keySet());
+        }
+
+        @Override
+        public Set<Map.Entry<Object, Object>> entrySet() {
+            Set<Map.Entry<Object, Object>> set = new LinkedHashSet<>((Set) map.entrySet());
+            return new LinkedHashSet<>(set);
+        }
+
+        @Override
+        public Collection<Object> values() {
+            return map.values();
+        }
+
+        @Override
+        public synchronized Object clone() {
+            Map<String, Object> m = new LinkedHashMap<>(map);
+            return new MapToPropertiesAdapter(m);
         }
     }
 }
