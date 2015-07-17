a9924a96ba53919c8a341bbb4e6275484a89f134
aalmiray <aalmiray@gmail.com>
2015-07-17 21:41:34 +0300

    Increase code coverage in griffon-core module (3)

---
+++ subprojects/griffon-core/src/test/groovy/griffon/core/i18n/NoSuchMessageExceptionSpec.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2008-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.core.i18n
+
+import spock.lang.Specification
+
+class NoSuchMessageExceptionSpec extends Specification {
+    void 'Exercise constructor with errors'() {
+        when:
+        new NoSuchMessageException(*args)
+
+        then:
+        thrown(exception)
+
+        where:
+        args          | exception
+        [null]        | IllegalArgumentException
+        ['key', null] | NullPointerException
+    }
+
+    void 'Successfully create exception'() {
+        when:
+        NoSuchMessageException exception = new NoSuchMessageException(*args)
+
+        then:
+        exception.key == args[0]
+        exception.locale == args[1] ?: Locale.default
+
+        where:
+        args << [['key'],['key', Locale.GERMAN]]
+    }
+}
