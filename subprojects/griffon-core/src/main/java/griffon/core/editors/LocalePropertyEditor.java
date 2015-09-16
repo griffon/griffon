0a5016083d28bb3ca974c0f4a1d577926fe25dba
aalmiray <aalmiray@gmail.com>
2015-09-16 16:43:20 +0200

    Add a Locale PropertyEditor and Formatter

---
+++ subprojects/griffon-core/src/main/java/griffon/core/editors/LocalePropertyEditor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2008-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.core.editors;
+
+import griffon.core.formatters.Formatter;
+import griffon.core.formatters.LocaleFormatter;
+
+import java.util.Locale;
+
+/**
+ * @author Andres Almiray
+ * @since 2.0.0
+ */
+public class LocalePropertyEditor extends AbstractPropertyEditor {
+    protected void setValueInternal(Object value) {
+        if (null == value) {
+            super.setValueInternal(null);
+        } else if (value instanceof Locale) {
+            super.setValueInternal(value);
+        } else if (value instanceof CharSequence) {
+            super.setValueInternal(resolveFormatter().parse(String.valueOf(value)));
+        } else {
+            throw illegalValue(value, Locale.class);
+        }
+    }
+
+    protected Formatter<Locale> resolveFormatter() {
+        return new LocaleFormatter();
+    }
+}
---
+++ subprojects/griffon-core/src/main/java/griffon/core/formatters/LocaleFormatter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.core.formatters;
+
+import javax.annotation.Nullable;
+import java.util.Locale;
+
+import static griffon.util.GriffonApplicationUtils.parseLocale;
+import static griffon.util.GriffonNameUtils.isBlank;
+
+/**
+ * @author Andres Almiray
+ * @since 2.5.0
+ */
+public class LocaleFormatter extends AbstractFormatter<Locale> {
+    @Nullable
+    public String format(@Nullable Locale locale) {
+        if (locale == null) return null;
+        StringBuilder b = new StringBuilder();
+        b.append(locale.getLanguage());
+        if (!isBlank(locale.getCountry())) {
+            b.append("_").append(locale.getCountry());
+            if (!isBlank(locale.getVariant())) {
+                b.append("_").append(locale.getVariant());
+            }
+        }
+
+        return b.toString();
+    }
+
+    @Nullable
+    @Override
+    public Locale parse(@Nullable String str) throws ParseException {
+        return parseLocale(str);
+    }
+}
