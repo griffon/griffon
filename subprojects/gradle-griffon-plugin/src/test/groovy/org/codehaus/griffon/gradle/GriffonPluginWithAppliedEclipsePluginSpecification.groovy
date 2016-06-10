fd1291008da8e6bbe70338c315a69936c2e88eea
aalmiray <aalmiray@gmail.com>
2016-06-10 22:57:41 +0200

    Upgrade dependencies

--- subprojects/gradle-griffon-plugin/src/test/groovy/org/codehaus/griffon/gradle/GriffonPluginWithAppliedEclipsePluginSpecification.groovy
+++ subprojects/gradle-griffon-plugin/src/test/groovy/org/codehaus/griffon/gradle/GriffonPluginWithAppliedEclipsePluginSpecification.groovy
@@ -25,7 +25,7 @@ class GriffonPluginWithAppliedEclipsePluginSpecification extends AbstractPluginS
             apply plugin: 'org.codehaus.griffon.griffon'
             apply plugin: 'eclipse'
             project.griffon {
-                version = '2.2.0'
+                version = '2.7.0'
             }
         }
     }
