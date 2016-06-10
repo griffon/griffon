74fc3bc0abd87bedf3a8b53222a554a43f9f3daa
aalmiray <aalmiray@gmail.com>
2016-06-10 18:24:45 +0200

    Added sample app showing Kotlin integration

---
+++ samples/sample-javafx-kotlin/griffon-app/views/sample/javafx/kotlin/SampleView.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2008-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.javafx.kotlin
+
+import griffon.core.artifact.GriffonController
+import griffon.core.artifact.GriffonView
+import griffon.metadata.ArtifactProviderFor
+import javafx.fxml.FXML
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.control.TextField
+import javafx.scene.paint.Color
+import javafx.stage.Stage
+import javafx.stage.Window
+import org.codehaus.griffon.runtime.javafx.artifact.AbstractJavaFXGriffonView
+
+@ArtifactProviderFor(GriffonView::class)
+class SampleView : AbstractJavaFXGriffonView() {
+    var model: SampleModel? = null
+    var controller: SampleController? = null
+
+    private @FXML var input: TextField? = null
+    private @FXML var output: Label? = null
+
+    override fun initUI() {
+        val stage: Stage = application.createApplicationContainer(mapOf()) as Stage
+        stage.title = application.configuration.getAsString("application.title")
+        stage.width = 400.0
+        stage.height = 120.0
+        stage.scene = _init()
+        application.getWindowManager<Window>().attach("mainWindow", stage)
+    }
+
+    private fun _init(): Scene {
+        val scene: Scene = Scene(Group())
+        scene.fill = Color.WHITE
+
+        val node = loadFromFXML()
+        model!!.inputProperty().bindBidirectional(input!!.textProperty())
+        model!!.outputProperty().bindBidirectional(output!!.textProperty())
+        (scene.root as Group).children.addAll(node)
+        connectActions(node as Any, controller as GriffonController)
+        return scene
+    }
+}
