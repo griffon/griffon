09ccaf821ff3972b110d78b671107acf74904715
aalmiray <aalmiray@gmail.com>
2016-12-12 18:07:11 +0100

    Add ElementObservableList

---
+++ subprojects/griffon-javafx/src/main/java/griffon/javafx/collections/ElementObservableList.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2008-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.javafx.collections;
+
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Andres Almiray
+ * @since 2.10.0
+ */
+public class ElementObservableList<E extends ElementObservableList.PropertyContainer> extends DelegatingObservableList<E> {
+    public interface PropertyContainer {
+        Property<?>[] properties();
+    }
+
+    private final Map<E, List<ListenerSubscription>> subscriptions = new LinkedHashMap<>();
+
+    public ElementObservableList(@Nonnull ObservableList<E> delegate) {
+        super(delegate);
+    }
+
+    @Override
+    protected void sourceChanged(@Nonnull ListChangeListener.Change<? extends E> c) {
+        while (c.next()) {
+            if (c.wasAdded()) {
+                c.getAddedSubList().forEach(this::registerListeners);
+            } else if (c.wasRemoved()) {
+                c.getRemoved().forEach(this::unregisterListeners);
+            }
+        }
+        fireChange(c);
+    }
+
+    private void registerListeners(@Nonnull E contact) {
+        if (subscriptions.containsKey(contact)) {
+            return;
+        }
+
+        List<ListenerSubscription> elementSubscriptions = new ArrayList<>();
+        for (Property<?> property : contact.properties()) {
+            elementSubscriptions.add(createChangeListener(contact, property));
+        }
+        subscriptions.put(contact, elementSubscriptions);
+    }
+
+    @Nonnull
+    @SuppressWarnings("unchecked")
+    private ListenerSubscription createChangeListener(@Nonnull final E contact, @Nonnull final Property<?> property) {
+        final ChangeListener listener = (observable, oldValue, newValue) -> fireChange(changeFor(contact));
+        property.addListener(listener);
+        return () -> property.removeListener(listener);
+    }
+
+    @Nonnull
+    private ListChangeListener.Change<? extends E> changeFor(@Nonnull final E contact) {
+        final int position = indexOf(contact);
+        final int[] permutations = new int[0];
+
+        return new ListChangeListener.Change<E>(this) {
+            private boolean invalid = true;
+
+            @Override
+            public boolean next() {
+                if (invalid) {
+                    invalid = false;
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void reset() {
+                invalid = true;
+            }
+
+            @Override
+            public int getFrom() {
+                return position;
+            }
+
+            @Override
+            public int getTo() {
+                return position + 1;
+            }
+
+            @Override
+            public List<E> getRemoved() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            protected int[] getPermutation() {
+                return permutations;
+            }
+
+            @Override
+            public boolean wasUpdated() {
+                return true;
+            }
+        };
+    }
+
+    private void unregisterListeners(@Nonnull E contact) {
+        List<ListenerSubscription> registeredSubscriptions = subscriptions.remove(contact);
+        if (registeredSubscriptions != null) {
+            registeredSubscriptions.forEach(ListenerSubscription::unsubscribe);
+        }
+    }
+
+
+    private interface ListenerSubscription {
+        void unsubscribe();
+    }
+}
---
+++ subprojects/griffon-javafx/src/test/groovy/griffon/javafx/collections/ElementObservableListTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2008-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package griffon.javafx.collections;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static javafx.collections.FXCollections.observableArrayList;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ElementObservableListTest {
+    @Test
+    public void testOperations() {
+        // given:
+        ObservableList<ObservablePerson> source = observableArrayList();
+        ObservableList<ObservablePerson> target = new ElementObservableList<>(source);
+        final AtomicBoolean changed = new AtomicBoolean(false);
+        ListChangeListener<ObservablePerson> listener = c -> changed.set(true);
+        target.addListener(listener);
+
+        // when:
+        ObservablePerson person = new ObservablePerson(1, "Andres", "Almiray");
+        source.add(person);
+
+        // then:
+        assertTrue(changed.get());
+
+        // when:
+        changed.set(false);
+        person.setLastname("Jaramillo");
+
+        // then:
+        assertTrue(changed.get());
+
+        // when:
+        source.remove(person);
+        changed.set(false);
+        person.setLastname("Almiray");
+
+        // then:
+        assertFalse(changed.get());
+    }
+
+
+    public static class ObservablePerson implements ElementObservableList.PropertyContainer {
+        private final IntegerProperty id = new SimpleIntegerProperty(this, "id");
+        private final StringProperty name = new SimpleStringProperty(this, "name");
+        private final StringProperty lastname = new SimpleStringProperty(this, "lastname");
+
+        public ObservablePerson(int id, String name, String lastname) {
+            setId(id);
+            setName(name);
+            setLastname(lastname);
+        }
+
+        public int getId() {
+            return id.get();
+        }
+
+        public IntegerProperty idProperty() {
+            return id;
+        }
+
+        public String getName() {
+            return name.get();
+        }
+
+        public StringProperty nameProperty() {
+            return name;
+        }
+
+        public String getLastname() {
+            return lastname.get();
+        }
+
+        public StringProperty lastnameProperty() {
+            return lastname;
+        }
+
+        public void setId(int id) {
+            this.id.set(id);
+        }
+
+        public void setName(String name) {
+            this.name.set(name);
+        }
+
+        public void setLastname(String lastname) {
+            this.lastname.set(lastname);
+        }
+
+        @Override
+        public Property<?>[] properties() {
+            return new Property<?>[]{
+                idProperty(),
+                nameProperty(),
+                lastnameProperty()
+            };
+        }
+    }
+}
