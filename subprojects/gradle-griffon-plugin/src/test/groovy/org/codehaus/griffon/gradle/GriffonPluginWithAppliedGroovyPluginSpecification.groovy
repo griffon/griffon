fa05a7158b33fe021b5c1734aa3d50ce9aca3ffa
aalmiray <aalmiray@gmail.com>
2017-09-04 21:07:40 +0200

    Bump version to next development cycle

--- subprojects/gradle-griffon-plugin/src/test/groovy/org/codehaus/griffon/gradle/GriffonPluginWithAppliedGroovyPluginSpecification.groovy
+++ subprojects/gradle-griffon-plugin/src/test/groovy/org/codehaus/griffon/gradle/GriffonPluginWithAppliedGroovyPluginSpecification.groovy
@@ -25,7 +25,7 @@ class GriffonPluginWithAppliedGroovyPluginSpecification extends AbstractPluginSp
             apply plugin: 'groovy'
             apply plugin: 'org.codehaus.griffon.griffon'
             project.griffon {
-                version = '2.12.0'
+                version = '2.12.1-SNAPSHOT'
             }
         }
     }
