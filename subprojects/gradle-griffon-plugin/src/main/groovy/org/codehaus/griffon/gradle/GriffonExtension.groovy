ddfa815b80ce1011d45631410210fc5ae32ee67d
aalmiray <aalmiray@gmail.com>
2018-05-31 16:17:10 +0200

    Bump version

--- subprojects/gradle-griffon-plugin/src/main/groovy/org/codehaus/griffon/gradle/GriffonExtension.groovy
+++ subprojects/gradle-griffon-plugin/src/main/groovy/org/codehaus/griffon/gradle/GriffonExtension.groovy
@@ -27,7 +27,7 @@ import org.gradle.api.Project
 class GriffonExtension {
     static final List<String> TOOLKIT_NAMES = ['swing', 'javafx', 'pivot', 'lanterna']
 
-    String version = '2.15.0-SNAPSHOT'
+    String version = '2.15.0'
 
     String toolkit
 
